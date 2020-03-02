0b7867b339a50d9d78b6db77a7293e1a2c34abe6
apapko <aliaksandr.papko2@ibm.com>
2020-03-02 14:10:43 +0300

    init: film list structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FilmListContainerComponent } from './film-list/film-list-container.component';
+import { FilmListComponent } from './film-list/components/film-list/film-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmListContainerComponent,
+    FilmListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/film-list/film-list-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-film-list',
+  templateUrl: './film-list.component.html',
+  styleUrls: ['./film-list.component.sass']
+})
+export class FilmListContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
